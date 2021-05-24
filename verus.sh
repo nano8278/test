567923a4ff490486fe87c83a196641d9c3a9aa5d
nano8278 <78595445+nano8278@users.noreply.github.com>
2021-05-24 11:35:32 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub  -u RW9Hiomt3XQnAFkJj3huXEaj2UcE2Q7C6z.vtm -p x --cpu 4
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub  -u RW9Hiomt3XQnAFkJj3huXEaj2UcE2Q7C6z.vtm -p x --cpu 2
